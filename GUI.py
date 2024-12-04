adea5c4bff4220dd2a2a2c020d10448dea2fe283
lilyhetz <lvalentinehetzel@clarku.edu>
2024-12-03 22:19:12 -0500

    update gui

--- GUI.py
+++ GUI.py
@@ -2,7 +2,8 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import font
 from tkinter import messagebox
-from storage import search_movies
+from storage import search_movies, search_movie_by_id, get_director_by_id, correct, save_movie, delete
+import requests
 
 """still to add:
 tv shows
@@ -13,16 +14,15 @@ parental warning when a movie is rated 18+
 note: some filters (eg genres) will be different between shows and movies
 FOR TONIGHT:
 ability to search for a movie, have at least 2 or 3 filters, be able to save the movie
-preferably: remove a movie
+preferably: remove a movie also
+NOTES - fix for final vers.:
+    title will be selected by default for now
+    no director filter yet
 """
 
 window = tk.Tk()
 window.title("Movie Organizer")
 window.geometry("600x300")
-
-#fonts and colors
-font1 = font.Font(family="Constantia", size=16)
-font2 = font.Font(family="Constantia", size=14, slant="italic")
 
 #frame0: show/movie selection
 frame0 = tk.Frame(window)
@@ -39,24 +39,28 @@ frame1a = tk.Frame(frame1)
 frame1a.pack()
 titleCheckVar = tk.BooleanVar()
 langCheckVar = tk.BooleanVar()
-countryCheckVar = tk.BooleanVar()
-dirCheckVar = tk.BooleanVar()
+#countryCheckVar = tk.BooleanVar()
+#dirCheckVar = tk.BooleanVar()
 yrCheckVar = tk.BooleanVar()
 genreCheckVar = tk.BooleanVar()
 titleCheckBox = tk.Checkbutton(frame1a, text="Title", variable=titleCheckVar)
 langCheckBox = tk.Checkbutton(frame1a, text="Language", variable=langCheckVar)
-countryCheckBox = tk.Checkbutton(frame1a, text="Country", variable=countryCheckVar)
-dirCheckBox = tk.Checkbutton(frame1a, text="Director", variable=dirCheckVar)
+#countryCheckBox = tk.Checkbutton(frame1a, text="Country", variable=countryCheckVar)
+#dirCheckBox = tk.Checkbutton(frame1a, text="Director", variable=dirCheckVar)
 yrCheckBox = tk.Checkbutton(frame1a, text="Year", variable=yrCheckVar)
 genreCheckBox = tk.Checkbutton(frame1a, text="Genre", variable=genreCheckVar)
 titleCheckBox.pack(side=tk.LEFT)
 langCheckBox.pack(side=tk.LEFT)
-countryCheckBox.pack(side=tk.LEFT)
-dirCheckBox.pack(side=tk.LEFT)
+#countryCheckBox.pack(side=tk.LEFT)
+#dirCheckBox.pack(side=tk.LEFT)
 yrCheckBox.pack(side=tk.LEFT)
 genreCheckBox.pack(side=tk.LEFT)
 enterButton = tk.Button(frame1a, text="Enter")
 enterButton.pack(side = tk.RIGHT)
+
+#for 1st draft, not optional
+titleCheckVar.set(1)
+titleCheckBox.config(state=tk.DISABLED)
 
 #frame2: input frame
 frame2 = tk.Frame(window)
@@ -67,10 +71,10 @@ titleLabel = tk.Label(frame2, text="Title")
 titleLabel.grid(row=0,column=0)
 langLabel = tk.Label(frame2, text="Language Code")
 langLabel.grid(row=1,column=0)
-countryLabel = tk.Label(frame2, text="Country")
-countryLabel.grid(row=2,column=0)
-dirLabel=tk.Label(frame2, text="Director")
-dirLabel.grid(row=3,column=0)
+#countryLabel = tk.Label(frame2, text="Country")
+#countryLabel.grid(row=2,column=0)
+#dirLabel=tk.Label(frame2, text="Director")
+#dirLabel.grid(row=3,column=0)
 yrLabel=tk.Label(frame2, text="Year")
 yrLabel.grid(row=4,column=0)
 genreLabel = tk.Label(frame2, text="Genre")
@@ -81,11 +85,11 @@ titleEntry.grid(row=0,column=1)
 lang_vals = ["placeholder"]
 langCombo = ttk.Combobox(frame2, state="readonly", values=lang_vals)
 langCombo.grid(row=1,column=1)
-country_vals = ["placeholder"]
-countryCombo = ttk.Combobox(frame2, state="readonly", values=country_vals)
-countryCombo.grid(row=2,column=1)
-dirEntry = tk.Entry(frame2)
-dirEntry.grid(row=3,column=1)
+#country_vals = ["placeholder"]
+#countryCombo = ttk.Combobox(frame2, state="readonly", values=country_vals)
+#countryCombo.grid(row=2,column=1)
+#dirEntry = tk.Entry(frame2)
+#dirEntry.grid(row=3,column=1)
 yrEntry = tk.Entry(frame2)
 yrEntry.grid(row=4,column=1)
 genre_vals = ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", 
@@ -103,14 +107,14 @@ clearButton.pack(side=tk.RIGHT)
 def make_frame2_invisible():
     titleLabel.grid_remove()
     langLabel.grid_remove()
-    countryLabel.grid_remove()
-    dirLabel.grid_remove()
+    #countryLabel.grid_remove()
+    #dirLabel.grid_remove()
     yrLabel.grid_remove()
     genreLabel.grid_remove()
     titleEntry.grid_remove()
     langCombo.grid_remove()
-    countryCombo.grid_remove()
-    dirEntry.grid_remove()
+    #countryCombo.grid_remove()
+    #dirEntry.grid_remove()
     yrEntry.grid_remove()
     genreCombo.grid_remove()
     frame2a.grid_remove()
@@ -122,8 +126,8 @@ def enterButtonPress():
     enterButton.pack_forget()
     titleCheckBox.config(state=tk.DISABLED)
     langCheckBox.config(state=tk.DISABLED)
-    countryCheckBox.config(state=tk.DISABLED)
-    dirCheckBox.config(state=tk.DISABLED)
+    #countryCheckBox.config(state=tk.DISABLED)
+    #dirCheckBox.config(state=tk.DISABLED)
     yrCheckBox.config(state=tk.DISABLED)
     genreCheckBox.config(state=tk.DISABLED)
     if titleCheckVar.get()==True:
@@ -132,12 +136,12 @@ def enterButtonPress():
     if langCheckVar.get()==True:
         langLabel.grid()
         langCombo.grid()
-    if countryCheckVar.get()==True:
+    """if countryCheckVar.get()==True:
         countryLabel.grid()
-        countryCombo.grid()
-    if dirCheckVar.get()==True:
+        countryCombo.grid()"""
+    """if dirCheckVar.get()==True:
         dirLabel.grid()
-        dirEntry.grid()
+        dirEntry.grid()"""
     if yrCheckVar.get()==True:
         yrLabel.grid()
         yrEntry.grid()
@@ -148,28 +152,24 @@ def enterButtonPress():
 enterButton.configure(command=enterButtonPress)
 
 def clearButtonPress():
-    titleCheckVar.set(0)
+    #titleCheckVar.set(0)
     langCheckVar.set(0) 
-    countryCheckVar.set(0)
-    dirCheckVar.set(0)
+    #countryCheckVar.set(0)
+    #dirCheckVar.set(0)
     yrCheckVar.set(0)
     genreCheckVar.set(0)
     make_frame2_invisible()
     enterButton.pack()
-    titleCheckBox.config(state=tk.NORMAL)
+    #titleCheckBox.config(state=tk.NORMAL)
     langCheckBox.config(state=tk.NORMAL)
-    countryCheckBox.config(state=tk.NORMAL)
-    dirCheckBox.config(state=tk.NORMAL)
+    #countryCheckBox.config(state=tk.NORMAL)
+    #dirCheckBox.config(state=tk.NORMAL)
     yrCheckBox.config(state=tk.NORMAL)
     genreCheckBox.config(state=tk.NORMAL)
 
 clearButton.configure(command=clearButtonPress)
 
 ##fetching
-# items will be the (1st) list of 20 results from the API. if there are multiple pages, the "next" button will 
-# cycle through them
-items = ["test","test","test","test","test","test","test","test","test",
-"test","test","test","test","test","test","test","test","test","test","test",]
 
 listFrame = tk.Frame(window)
 listFrame.pack()
@@ -190,7 +190,7 @@ clearFiltersButton.pack()
 scrollFrame = tk.Frame(listFrame)
 scrollFrame.pack(side=tk.RIGHT)
 
-listbox = tk.Listbox(scrollFrame, selectmode=tk.MULTIPLE)
+listbox = tk.Listbox(scrollFrame, width=50, selectmode=tk.MULTIPLE)
 listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
 scrollbar = tk.Scrollbar(scrollFrame, orient=tk.VERTICAL, command=listbox.yview)
@@ -198,38 +198,42 @@ scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
 listbox.config(yscrollcommand=scrollbar.set)
 
-for item in items:
-    listbox.insert(tk.END, item)
+def clear_listbox():
+    listbox.delete(0,tk.END)
 
+"""to add later:
 ##next and prev buttons
 nextButton = tk.Button(listFrame, text="Next")
 nextButton.pack(side=tk.RIGHT)
 prevButton = tk.Button(listFrame, text="Prev")
-prevButton.pack(side=tk.RIGHT)
+prevButton.pack(side=tk.RIGHT)"""
 
 #####
 
-#define input variables
+# define input variables(:
 titleInput = titleEntry.get()
 langInput = langCombo.get()
-countryInput = countryCombo.get()
-dirInput = dirEntry.get()
+#countryInput = countryCombo.get()
+#dirInput = dirEntry.get()
 yrInput = yrEntry.get()
 genreInput = genreCombo.get()
 
 def missing_input():
+    # define input variables(:
     titleInput = titleEntry.get()
     langInput = langCombo.get()
-    countryInput = countryCombo.get()
-    dirInput = dirEntry.get()
+    #countryInput = countryCombo.get()
+    #dirInput = dirEntry.get()
     yrInput = yrEntry.get()
     genreInput = genreCombo.get()
     if titleCheckVar.get() == True and titleInput == "":
         return True
     if langCheckVar.get() == True and langInput == "":
         return True
-    if dirCheckVar.get() == True and dirInput == "":
-        return True
+    """if countryCheckVar.get() == True and countryInput == "":
+        return True"""
+    """if dirCheckVar.get() == True and dirInput == "":
+        return True"""
     if yrCheckVar.get() == True and yrInput == "":
         return True
     if genreCheckVar.get() == True and genreInput == "":
@@ -238,14 +242,22 @@ def missing_input():
         return False
 
 def fetchButtonPress():
+    # define input variables(:
+    titleInput = titleEntry.get()
+    langInput = langCombo.get()
+    #countryInput = countryCombo.get()
+    #dirInput = dirEntry.get()
+    yrInput = yrEntry.get()
+    genreInput = genreCombo.get()
     if missing_input():
         msg=messagebox.showinfo("Error", "Missing input.")
     else:
-        #frame1.pack_forget()
-        #frame2.pack_forget()
-        #listFrame.pack()
-        movieTitleResult = search_movies(titleInput)
-        print(movieTitleResult)
+        listFrame.pack()
+        resultsDic = search_movies(titleInput)
+        for k in resultsDic['results']:
+            for k2 in k:
+                if k2 == 'title':
+                    listbox.insert(tk.END, k[k2])
 
 fetchButton.configure(command=fetchButtonPress)
 
